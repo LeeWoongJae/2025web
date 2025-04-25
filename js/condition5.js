0a79f33cc61f73128df873a38d8d47cd1a40cf85
LeeWJ <dsa00340@naver.com>
2025-04-25 17:52:11 +0900

    homework

---
+++ js/condition5.js
@@ -0,0 +1,27 @@
+// let num1 = prompt("숫자를 입력하세요 [1 ~ 3 까지]");
+// switch (num1) {
+//   case "1":
+//     console.log("1을 선택");
+//     break;
+//   case "2":
+//     console.log("2을 선택");
+//     break;
+//   case "3":
+//     console.log("3을 선택");
+//     break;
+//   default:
+//     console.log("1 ~ 3중에서만 선택해주세요!");
+// }
+
+
+let result = 30 + Math.floor(Math.random()* 71);
+console.log(result);
+result = parseInt(result /10);
+switch(result){
+  case result = 10:console.log("S"); break;
+  case result = 9:console.log("A"); break;
+  case result = 8:console.log("B"); break;
+  case result = 7:console.log("C"); break;
+  case result = 6:console.log("D"); break;
+  default : console.log('F 입니다');
+}
